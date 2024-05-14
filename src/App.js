2ef053c96d8e111e60093676c515b97d642dc32e
Park Sung Jin <lbon321@naver.com>
2024-05-14 20:07:03 +0900

    2024-05-14

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import WiskyList from './pages/wisky';
-import List from './pages/review';
+// import List from './pages/review';
+import MyComponent from './pages/css';
 function App() {
   return (
    <>
-    <List />
+    <MyComponent />
    </>
   );
 }
